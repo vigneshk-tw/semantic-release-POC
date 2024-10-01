dd81c1537d6db90c1fe86a36bb5c5aa32744439d
sangeet-joy_xero <sangeet.joy@xero.com>
2024-10-01 15:22:26 +0530

    fix: Added custom release notes

--- .github/semantic-release/release.config.js
+++ .github/semantic-release/release.config.js
@@ -3,7 +3,13 @@ const config = {
     tagFormat: "${version}",
     Plugins: [
         '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        ['@semantic-release/release-notes-generator', {
+            "preset": "angular",
+            "writerOpts": {
+                "headerPartial": "#{{version}}\n",
+                "footerPartial": "\n**Full Changelog**:https://github.com/XeroAPI/Xero-OpenAPI/compare/{{previousTag}}...{{version}}"
+            }
+        }],
         '@semantic-release/changelog',
         '@semantic-release/github',
         '@semantic-release/git',
